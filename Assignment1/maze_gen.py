9e177dd9d0cc6362081d6af9ce99f07d48f1971d
mshachnai <m.shachnai@rutgers.edu>
2019-02-04 01:36:11 -0500

    updated

--- Assignment1/maze_gen.py
+++ Assignment1/maze_gen.py
@@ -1,5 +1,4 @@
 #Maze Generator -- INTRO TO AI 198:520 -- Rutgers University -- M.Shachnai
-
 from tkinter import *
 from tkinter import ttk
 import random
@@ -7,28 +6,33 @@ import random
 #maze generator will be formed using a 2d array of struct named cell
 class Cell:
     #instance variables unique to each instance (with default arguments)
-    def __init__(self, val = 0, heuri = 0): 
-        self.val = val      
-        self.heuri = heuri
-
-#function to generate maze - takes in dimension of maze, probability of blocked
-#cell, and 2d array
+    def __init__(self, val = 0, heuri = 0, coord = 0): 
+        self.val = val     #value to denote open/blocked cell
+        self.heuri = heuri #heuristic value 
+        self.coord = coord #this is touple to indicate cell coordinates
+    
+    
+#function to generate maze - takes in dimension of maze, probability of blocked cell, and 2d array
 def maze_gen(dim, prob, maze):
     random.seed()
-
-    #construct 2d array from cells and assign white/black cells based on
-    #given probability
-    maze = []
     for i in range(0,dim):
         maze.append([])
-
+    
     for i in range(0,dim):
         for j in range(0,dim):
             rand = random.random()  #used for generating a value between 0-1
-            if rand <= prob :
-                maze[i].append(Cell(val = 1)) #blocked cell
+            if rand <= prob and (i != 0 or j != 0) and (i != dim-1 or j !=
+                    dim-1) :
+                maze[i].append(Cell(val = 1, coord = (i,j))) #blocked cell
+                #print(maze[i][j].coord)     #coordinates of blocked cells
             else : 
-                maze[i].append(Cell(val = 0)) #open cell
+                maze[i].append(Cell(val = 0, coord = (i,j))) #open cell
+                #print(maze[i][j].coord)     #coordinates of open cells cells
+    return
+
+#function to generate a visual of the maze and its solution
+######need to work on showing actual path taken
+def maze_visual(dim, maze):
 
     #initialize visual window and create maze layout using buttons
     root = Tk()
@@ -53,30 +57,30 @@ def maze_gen(dim, prob, maze):
                 button4 = Button(root, relief = SOLID, borderwidth = 1, bg = "white", width = 1).grid(row=r,column=c)
 
     root.mainloop()
-    return;
-
-####### this will be in main function of program  ######
-#take in user input of maze dimension and blocked cell probability
-dim = int(input("Enter dimension: "))
-prob = float(input("Enter probability: "))
-
-#initialize list which forms basis for 2d maze
-maze = []
-maze_gen(dim, prob, maze)
+    return
 
 
+def main():
 
+    #take in user input of maze dimension and blocked cell probability
+    dim = int(input("Enter dimension: "))
+    prob = float(input("Enter probability: "))
 
-######## for later use potentially
-#for i in range(0,dim):
-#    maze.append([])
-#    
-#for i in range(0,dim):
-#    for j in range(0,dim):
-#        maze[i].append(Cell())
+    #initialize list which forms basis for 2d maze
+    maze = []
 
-#for i in range(dim) : print(maze[i][dim-1].val)
-#print(for i in range(dim) : maze[i][n].val)
-#c1 = Cell(2,1)
-#print(c1.val, c1.heuri)
+    #run maze generator
+    maze_gen(dim, prob, maze)
 
+    #run search algorithm
+    ####insert here####
+    #if there is a path - show it with maze_visual
+    #otherwise print("no solution")
+
+    #generate maze visual
+    maze_visual(dim, maze)
+
+    #print(maze[dim-1][dim-1].val)
+    return
+
+main()
