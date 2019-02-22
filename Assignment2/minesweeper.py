699ca88e0e5e92cff37e843a19b8b490a8a8fd0b
mshachnai <m.shachnai@rutgers.edu>
2019-02-22 18:03:33 -0500

    nearly finished

--- Assignment2/minesweeper.py
+++ Assignment2/minesweeper.py
@@ -6,7 +6,7 @@ import timeit as tm
 import random
 import copy
 
-#DEBUG is used for turning visuals on/off:
+#DEBUG is used for turning visuals on/off: (not complete yet)
 #0: will not show any visuals
 #1: will show grid visuals
 #2: will show plotting graphs
@@ -17,10 +17,9 @@ RUNS = 1 #number of times each algorithm is run for timing
 #minesweeper will be formed using a 2d array of struct named cell
 class Cell:
     #instance variables unique to each instance (with default arguments)
-    def __init__(self, val = 0, heuri = 0, coord = (0,0), bomb = 0): 
+    def __init__(self, val = 0, coord = (0,0), bomb = 0): 
         self.val = val     #value to denote nearby bombs
         self.bomb = bomb     #value to denote if bomb/clear
-        self.heuri = heuri #heuristic value 
         self.coord = coord #this is touple to indicate cell coordinates
     
 #function to add values to each cell according num of adjacent mines
@@ -32,12 +31,11 @@ def cell_val(dim, grid = [], X = 0, Y = 0, depth = 0):
         return 0
     #check if X/Y coordinates are out of range 
     elif X < 0 or X >= dim or Y < 0 or Y >= dim: 
-        #depth += 1
         #print("out of range")
         return 0
+    #check if coordinate is a bomb and increment accordingly
     elif grid[X][Y].bomb == 1:
-        #print("this is bomb coordinat", X, Y)
-        #depth += 1
+        #print("this is bomb coordinate", X, Y)
         return 1
         
     depth += 1
@@ -70,49 +68,47 @@ def mine_gen(dim, num_mines):
         
         #count number of mines placed
         count += 1
-
+    
+    #add cell value based on adjacent bombs
     for i in range(dim):
         for j in range(dim):
             grid[i][j].val = cell_val(dim, grid, X = i, Y = j)
     
     return grid
 
+
+
 #function to generate a visual of the grid and its solution if given
 def grid_visual(dim, grid, sol = []):
 
     #initialize visual window and create grid layout using buttons
     root = Tk()
-    root.title('Maze Runner')
+    root.title('Minesweeper')
+  
+    #clickable function for minesweeper
+    def mouse_press(n, row, col):
+        #print("hello")
+        if grid[row][col].bomb == 1:
+            button[n].config(bg = "red", command = 0, relief = SUNKEN, text =
+                    "X", state = DISABLED)    
+        else: 
+            button[n].config(relief = SUNKEN, command = 0, text =
+                    grid[row][col].val, state = DISABLED, disabledforeground =
+                    "blue")    
+    button = []
+    count = 0
 
     for r in range(dim): #width of grid
         for c in range(dim): #height of grid
 
-            #If cell contains bomb, display it
-            if grid[r][c].bomb == 1:
-                button1 = Button(root, text = "X", relief = SOLID, borderwidth =
-                        1, bg = "red", height = 1, width = 1 ).grid(row=r,column=c)
-
-                """
-            #bottom right cell of grid will be named 'G' - goal
-            elif r == dim-1 and c == dim-1 :  
-                button2 = Button(root, text = "G", command = root.destroy, relief = SOLID, borderwidth = 1, bg = "light blue", width = 1 ).grid(row=r,column=c)
-
-            #if cell value == 1, this is a blocked cell
-            elif grid[r][c].val == 1:
-                button3 = Button(root, relief = SOLID, state = DISABLED, borderwidth = 1, bg = "black", width = 1).grid(row=r,column=c)
-                        
-            #mark path on the grid visual by checking the cell coordinate is
-            #listed in the solution array, if it is - mark the specific cell
-            elif (r,c) in sol:
-                button4 = Button(root, relief = SOLID, state = DISABLED,
-                borderwidth = 1, bg = "yellow", width =
-                1).grid(row=r,column=c)"""
-                        
-            #for all other cases, create blank cells
-            else:
-                button5 = Button(root, relief = SOLID, text = grid[r][c].val,
-                        borderwidth = 1, bg = "light blue", width = 1).grid(row=r,column=c)
-
+            #create blank clickable cells
+            #else:
+            button.append(Button(root, relief = SOLID, text = "",
+                    command = lambda n = count, row = r, col = c :
+                    mouse_press(n, row, col), borderwidth = 1, bg = "light grey", width = 1))
+            button[-1].grid(row=r,column=c)
+    
+            count += 1
     root.mainloop()
     return
 
@@ -127,60 +123,24 @@ def main():
     grid = mine_gen(dim, num_mines)
     grid_visual(dim, grid)
 
-    #2)run search algorithm and generate grid visual
-    #if there is a path - show it with grid_visual
-    #otherwise print("No path")
-    """print("A* Euclidean")
-    res = AStarE(grid)
-    print(tm.timeit(lambda: AStarE(grid), number = RUNS))
-    if DEBUG == 1 or DEBUG == 3 :
-        if res is None : 
-            print("No path")
-        else : 
-            grid_visual(dim, grid, res[0])
+    #2)run agent to solve the maze (collect, update KB, take action) 
 
-    print("A* Manhattan")
-    res = AStarM(grid)
-    print(tm.timeit(lambda: AStarM(grid), number = RUNS))
-    if DEBUG == 1 or DEBUG == 3 :
-        if res is None : 
-            print("No path")
-        else : 
-            grid_visual(dim, grid, res[0])
 
-    print("BFS")
-    res = BFS(grid)
-    print(tm.timeit(lambda: BFS(grid), number = RUNS))
-    if DEBUG == 1 or DEBUG == 3 :
-        if res is None : 
-            print("No path")
-        else : 
-            grid_visual(dim, grid, res[0])
-
-    print("DFS")
-    res = DFS(grid)
-    print(tm.timeit(lambda: DFS(grid), number = RUNS))
-    if DEBUG == 1 or DEBUG == 3 :
-        if res is None : 
-            print("No path")
-        else : 
-            grid_visual(dim, grid, res[0])
-
-    #4)plot algorithm stats with graphs
+    #3)plot agent stats with graphs
     #density vs. solvability
-    array = [1,2,3,4]
-    plt.plot(array, [1, 2, 3, 4], 'ro')
-    plt.ylabel('density')
-    plt.xlabel('solvability')
-    if DEBUG == 2 or DEBUG == 3 :
-        plt.show()
-    
-    #density vs. shortest expected path
-    plt.plot([1,2,3,4], [1, 2, 7, 8], 'ro')
-    plt.ylabel('density')
-    plt.xlabel('shortest expected path')
-    if DEBUG == 2 or DEBUG == 3:
-        plt.show()"""
+    #array = [1,2,3,4]
+    #plt.plot(array, [1, 2, 3, 4], 'ro')
+    #plt.ylabel('density')
+    #plt.xlabel('solvability')
+    #if DEBUG == 2 or DEBUG == 3 :
+    #    plt.show()
+    #
+    ##density vs. shortest expected path
+    #plt.plot([1,2,3,4], [1, 2, 7, 8], 'ro')
+    #plt.ylabel('density')
+    #plt.xlabel('shortest expected path')
+    #if DEBUG == 2 or DEBUG == 3:
+    #    plt.show()"""
 
     return
 
