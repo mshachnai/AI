3fe72bc570cf0be2520d7782e679f8b5ac42a812
KarthikPaka <karthik97p@gmail.com>
2019-05-06 16:05:34 -0400

    refactor

--- Assignment4/neuralNet.py
+++ Assignment4/neuralNet.py
@@ -2,12 +2,6 @@ import os
 from PIL import Image
 import numpy as np
 
-def sigmoid(x):
-    return 1.0/(1+ np.exp(-x))
-
-def sigmoid_derivative(x):
-    return x * (1.0 - x)
-
 class NeuralNetwork:
     def __init__(self, x, y):
         self.input      = x
@@ -17,14 +11,19 @@ class NeuralNetwork:
         self.output     = np.zeros(3)
 
     def feedforward(self):
-        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
-        self.output = sigmoid(np.dot(self.layer1, self.weights2))
+        self.hiddenLayer = sigmoid(np.dot(self.input, self.weights1))
+        self.output = sigmoid(np.dot(self.hiddenLayer, self.weights2))
 
     def backprop(self):
 
-        self.weights2 += np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
-        self.weights1 += np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
+        self.weights2 += np.dot(self.hiddenLayer.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
+        self.weights1 += np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.hiddenLayer)))
 
+def sigmoid(x):
+    return 1.0/(1+ np.exp(-x))
+
+def sigmoid_derivative(x):
+    return x * (1.0 - x)
 
 if __name__ == "__main__":
 
@@ -33,33 +32,35 @@ if __name__ == "__main__":
                         [0,0,0]]),
                        
                         np.array( [0,0,0] )   )
-    for k in range(20):
-        photo = Image.open('%d.jpg' % k)
-        photo = photo.convert('RGB')
-        grayVersion = photo.convert('L')
-        grayMatrix = np.array(grayVersion)
 
-        width = photo.size[0] #define W and H
-        height = photo.size[1]
+    for l in range(20):
+        for k in range(20):
+            photo = Image.open('%d.jpg' % k)
+            photo = photo.convert('RGB')
+            grayVersion = photo.convert('L')
+            grayMatrix = np.array(grayVersion)
 
-        #training our NN with all the pixel sets of a certain image
-        for j in range(1, height-1): #each pixel's coordinates
+            width = photo.size[0] #define W and H
+            height = photo.size[1]
 
-            for i in range(1, width-1):
+            #training our NN with all the pixel sets of a certain image
+            for j in range(1, height-1): #each pixel's coordinates
 
-                RGB = photo.getpixel((i,j))
-                R,G,B = RGB  #current pixel's RGB value
+                for i in range(1, width-1):
 
-                inputMatrix = np.array([[grayMatrix[j-1][i-1],grayMatrix[j-1][i],grayMatrix[j-1][i+1]],
-                                        [grayMatrix[j][i-1],grayMatrix[j][i],grayMatrix[j][i+1]],
-                                        [grayMatrix[j+1][i-1],grayMatrix[j+1][i],grayMatrix[j+1][i+1]]])
-                actualRGB = np.array([R,G,B])
+                    RGB = photo.getpixel((i,j))
+                    R,G,B = RGB  #current pixel's RGB value
 
-                nn.input = inputMatrix
-                nn.y = actualRGB
+                    inputMatrix = np.array([[grayMatrix[j-1][i-1],grayMatrix[j-1][i],grayMatrix[j-1][i+1]],
+                                            [grayMatrix[j][i-1],grayMatrix[j][i],grayMatrix[j][i+1]],
+                                            [grayMatrix[j+1][i-1],grayMatrix[j+1][i],grayMatrix[j+1][i+1]]])
+                    actualRGB = np.array([R,G,B])
 
-                nn.feedforward()
-                nn.backprop()
+                    nn.input = inputMatrix
+                    nn.y = actualRGB
+
+                    nn.feedforward()
+                    nn.backprop()
 
     #running our NN on a given input
     photo = Image.open('2.jpg')
